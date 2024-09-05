a76061f476f977cbbeb870cbeba0d564b8a63c55
Maclean <contatolucasmaclean@gmail.com>
2024-09-04 22:51:45 -0300

    infra: upload branch feat/Maclean

--- src/app/planos/page.tsx
+++ src/app/planos/page.tsx
@@ -1,7 +1,9 @@
 export default function Page() {
+  const x = 'Gustavo'
+
   return (
     <>
-      <div className="flex flex-col text-amber-400">Planoss</div>
+      <div className="flex flex-col text-amber-400">Planoss {x}</div>
     </>
   )
 }
